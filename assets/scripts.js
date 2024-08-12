b4f92af87d42ea298d3d0e55300a2c232432976c
Matt-at-CS <matt.bedford@classroomsecrets.co.uk>
2024-08-12 11:48:44 +0200

    fixed like class swicth

--- assets/scripts.js
+++ assets/scripts.js
@@ -7,9 +7,18 @@ document.addEventListener('DOMContentLoaded', function() {
 			event.preventDefault();
 			let ref = event.target.closest('.likes-wrapper');
 			let ProdId = ref.dataset.productId;
+			switchLikeStatus(ref);
 			doLike(ProdId, ref);
 		});
 	});
+	
+	function switchLikeStatus(ref) {
+        if(ref.classList.contains('liked')) {
+            ref.classList.remove('liked');
+        } else {
+            ref.classList.add('liked');
+        }
+    }
 
     function doLike(ProdId, ref) {
         let url = `/wp-json/dwl/v1/like/${ProdId}`;
