0df9f2ebddb1cc8f2fae0b43dbd67acf1235782a
Matt-at-CS <matt.bedford@classroomsecrets.co.uk>
2024-08-30 15:59:59 +0200

    Final working changes

--- cookie.php
+++ cookie.php
@@ -19,14 +19,19 @@ class Cookie
     public static function cookieToMeta($user_string, $user_obj)
     {
         if (!isset($_COOKIE['dwl_liked_products'])) {
+            Logger("We could not find cookie so we are bailing.");
           	return;
         }
         
         $liked_products = json_decode($_COOKIE['dwl_liked_products']);
         $user_id = $user_obj->ID;
+      	Logger("user id is: " . $user_id);
         if(0 === $user_id) return;
         $user_liked_products = get_user_meta($user_id, 'liked_products', true);
         $user_liked_products = empty($user_liked_products) ? [] : $user_liked_products;
+
+      	Logger("liked_products = " . json_encode($liked_products));
+      	Logger("user liked_products = " . json_encode($user_liked_products));
       
         foreach ($liked_products as $product_id) {
             if (!in_array($product_id, $user_liked_products)) {
@@ -37,4 +42,5 @@ class Cookie
         setcookie('dwl_liked_products', json_encode([]), time() + (86400 * 30), "/");
     }
 }
+
 
--- init.php
+++ init.php
@@ -3,8 +3,8 @@
 /**
  * Plugin Name: Do Woo Like
  * Description: A Woo Commerce plugin that allows users to like products.
- * Version:     1.0.0
- * Author:      Dev Team
+ * Version:     1.6.0
+ * Author:      Matt Bedford
  * Text Domain: do-woo-like
  * License:     GPL v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
@@ -38,4 +38,5 @@ class initDoWooLike
     }
 }
 add_action('init', [new initDoWooLike(), 'Like']);
+
 
